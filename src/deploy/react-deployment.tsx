cd5de43f2ea6a38a30d78c027360f94d7e500f87
Emmanuel <emmanuel.davidson@thejitu.com>
2022-10-09 20:55:37 +0300

    update react deployment

--- src/deploy/react-deployment.tsx
+++ src/deploy/react-deployment.tsx
@@ -9,12 +9,23 @@ import dataFunctions from '../data';
 const ReactDeployment = () => {
 
     const [projectName, setProjectName] = React.useState("");
+    const [deployData, setDeployData] = React.useState({});
+    const [done, setDone] = React.useState(false);
 
 
-    const clickHandler = () => {
+    const clickHandler = async () => {
         console.log(projectName);
-        const data = dataFunctions.deploy({payload: {projectName}, path: "deploy/react"});
-        console.log(data);
+        try{
+            const data = await dataFunctions.deploy({payload: {
+                projectName,
+                app: "react"
+            }, path: "deploy/"});
+            console.log(data);
+            setDeployData(data);
+            setDone(true);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,19 +34,19 @@ const ReactDeployment = () => {
     }
     return (
         <div className="content border d-flex flex-column  w-100  justify-content-center align-self-start m-5 p-5">
-            <Form.Label>Project name</Form.Label>
-            <Form.Control
+           {!done && <Form.Label>Project name</Form.Label>}
+           {!done && <Form.Control
                 type="text"
                 id="project-name"
                 aria-describedby="project-name"
                 placeholder="Enter project name"
                 onChange={changeHandler}
-            />
-            <Form.Text id="project-name" muted>
+            />}
+            {!done && <Form.Text id="project-name" muted>
                 Please enter a name for your project
-            </Form.Text>
+            </Form.Text>} 
 
-            <Button onClick={clickHandler} className="w-25 m-2 align-self-end" variant="success" size='sm'>Submit</Button>
+            {!done &&<Button onClick={clickHandler} className="w-25 m-2 align-self-end" variant="success" size='sm'>Submit</Button>}
         </div>
     );
 }
